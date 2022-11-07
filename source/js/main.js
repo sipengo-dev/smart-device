0bdef81509d00b07c45ead82c894133ca5763aa9
Елизавета Яценко <sipengodev@gmail.com>
2022-11-07 23:14:17 +0300

    create mask phone

--- source/js/main.js
+++ source/js/main.js
@@ -1,8 +1,13 @@
 import {openModal, closeModal} from './modules/modal.js';
 import {handleButtonClick} from './modules/consultation.js';
 import {buttonDetailsText, openContent} from './modules/additionally.js';
-import {toggleAccordionPoints, toggleAccordionContacts, accordionButtonContacts, accordionButtonPoints} from './modules/accordion.js';
-
+import {
+  toggleAccordionPoints,
+  toggleAccordionContacts,
+  accordionButtonContacts,
+  accordionButtonPoints
+} from './modules/accordion.js';
+import IMask from 'imask';
 
 const buttonOpenModal = document.querySelector('.main-header__feedback-btn');
 const buttonCloseModal = document.querySelector('.modal__close-btn');
@@ -19,3 +24,14 @@ accordionButtonPoints.addEventListener('click', toggleAccordionPoints);
 accordionButtonContacts.addEventListener('click', toggleAccordionContacts);
 
 buttonDetailsText.addEventListener('click', openContent);
+
+const phoneInputs = document.querySelectorAll('.phone');
+
+phoneInputs.forEach((phoneInput) => {
+  const phoneMask = new IMask(phoneInput, {
+    mask: '+{7} (000) 000-00-00',
+  });
+  phoneInput.addEventListener('input', phoneMask);
+});
+
+
