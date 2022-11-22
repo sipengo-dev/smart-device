0aeaa1a09a4fcfa777c5798e0d48e02553bfdb15
Елизавета Яценко <sipengodev@gmail.com>
2022-11-22 22:55:30 +0300

    bugs fixed

--- source/js/modules/modal.js
+++ source/js/modules/modal.js
@@ -1,15 +1,26 @@
+import {filterStageFromList} from '@babel/preset-env/lib/utils';
+
 const modal = document.querySelector('#modalFeedback');
+const body = document.querySelector('.body');
+const app = document.querySelector('#app');
 
 function onModalEscKeydown(evt) {
+  if (evt.keyCode === 9 && evt.srcElement.tabIndex === 6) {
+    modal.querySelector('.modal__feedback').focus();
+  }
   if (evt.keyCode === 27) {
     evt.preventDefault();
     closeModal();
   }
 }
+
 function openModal() {
   if (modal.classList.contains('modal-close')) {
     modal.classList.remove('modal-close');
     document.addEventListener('keydown', onModalEscKeydown);
+    body.classList.add('scroll-lock');
+    app.inert = true;
+    modal.querySelector('.modal__feedback').focus();
   }
 }
 
@@ -17,7 +28,16 @@ function closeModal() {
   if (!modal.classList.contains('modal-close')) {
     modal.classList.add('modal-close');
     document.removeEventListener('keydown', onModalEscKeydown);
+    body.classList.remove('scroll-lock');
+    app.inert = false;
   }
 }
+
+modal.addEventListener('click', function (evt) {
+  if (evt.target.id === 'modalFeedback') {
+    closeModal();
+  }
+});
+
 
 export {openModal, closeModal};
